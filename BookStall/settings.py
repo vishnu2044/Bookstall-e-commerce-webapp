2481b224145c6a424671e667ca68794c3fcbc476
vishnu narayanan <vishnunarayanan2044@gmail.com>
2024-03-22 14:20:17 +0530

    static root added

--- BookStall/settings.py
+++ BookStall/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # Static files configuration
 import os
 # STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
@@ -169,6 +169,7 @@ import os
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
